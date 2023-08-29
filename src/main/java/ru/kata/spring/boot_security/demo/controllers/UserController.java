63617ece6451813e0450a0f8275bb26347b91bac
Dmitry Khakhalev <khakhalev@mail.ru>
2023-08-30 01:52:29 +0300

    добавить контроллер АДМИНСКИЙ. убрать старый HELLO. доделать вьюшки!

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,29 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.services.UserServices;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserServices userServices;
+
+    @Autowired
+    public UserController(UserServices userServices) {
+        this.userServices = userServices;
+    }
+
+
+    @GetMapping("/user")
+    public String showUser(Model model, Principal principal) {
+        model.addAttribute("user", userServices.findByUsername(principal.getName()));
+        return "user";
+    }
+
+
+}
