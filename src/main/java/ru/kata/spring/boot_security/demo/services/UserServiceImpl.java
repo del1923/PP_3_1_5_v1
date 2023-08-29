c601110eeb11d73292ec540c9f492f3a56052c7b
Dmitry Khakhalev <khakhalev@mail.ru>
2023-08-30 01:43:49 +0300

    Добавлена RoleServiceImpl Переделать вьюшки!

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package ru.kata.spring.boot_security.demo.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserServices {
@@ -16,4 +21,38 @@ public class UserServiceImpl implements UserServices {
     }
 
 
+    @Override
+    public User findByUsername(String name) {
+        return null;
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+
+    @Override
+    public User showUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
