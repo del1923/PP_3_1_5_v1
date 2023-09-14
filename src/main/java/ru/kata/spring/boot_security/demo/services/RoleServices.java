be7e64d1c0decf8e9ce2424cb8e302892b2fb765
Dmitry Khakhalev <khakhalev@mail.ru>
2023-09-14 23:03:53 +0300

    Внесены исправления RoleServices Добавлены модальные окна (не работают!)

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleServices.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServices.java
@@ -2,9 +2,9 @@ package ru.kata.spring.boot_security.demo.services;
 
 import ru.kata.spring.boot_security.demo.model.Role;
 
-import java.util.List;
+import java.util.Set;
 
 public interface RoleServices {
-    List<Role> getAllRoles();
+    Set<Role> getAllRoles();
 
 }
